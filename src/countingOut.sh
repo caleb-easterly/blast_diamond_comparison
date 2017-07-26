8a8495de3c9331ad62e60ed5a6447dad46abfe6c
caleb-easterly <easte080@umn.edu>
2017-07-26 11:34:59 -0500

    temp to temp folder

--- src/countingOut.sh
+++ src/countingOut.sh
@@ -1,6 +1,6 @@
 datafile=out/$1.tab
 timefile=out/$1.time
-awk '{print $1}' $datafile | sort -u > seq.temp
+awk '{print $1}' $datafile | sort -u > temp/seq.temp
 
 numMatches=$(wc -l $datafile | awk '{print $1}')
 uniqQuery=$(cat seq.temp | wc -l)
@@ -8,4 +8,4 @@ uniqAccessions=$(awk '{print $2}' $datafile | sort -u | wc -l)
 walltime=$(cat $timefile)
 
 echo -e "$numMatches\t$uniqQuery\t$uniqAccessions\t$walltime"
-rm seq.temp
+rm temp/seq.temp
