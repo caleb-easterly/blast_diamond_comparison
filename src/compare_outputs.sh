bb93b082b6fb940a377c14ecaa28f5fbaf3a3afe
caleb-easterly <easte080@umn.edu>
2017-07-26 13:56:48 -0500

    allow for passing method and number (e.g., blast1)

--- src/compare_outputs.sh
+++ src/compare_outputs.sh
@@ -1,8 +1,11 @@
 # first argument is diamond output, second is blast output
 
+diamondDataFile=out/$1.tab
+blastDataFile=out/$2.tab
+
 # basic data on blast
-blastNumMatches=$(wc -l $2 | awk '{print $1}')
-blastUniqQuery=$(awk '{print $1}' $2 | sort -u | wc -l)
+blastNumMatches=$(wc -l $diamondDataFile | awk '{print $1}')
+blastUniqQuery=$(awk '{print $1}' $blastDataFile | sort -u | wc -l)
 
 # ----Comparisons----
 
@@ -11,13 +14,17 @@ blastUniqQuery=$(awk '{print $1}' $2 | sort -u | wc -l)
 #+	one common alignment divided by the number of reads for which Blast finds 
 #+	at least one alignment (percentage of queries mapped)
 # sorted queries and accessions
-cut -d\| -f2 $2 > temp/acc && cut -f1 $2 > temp/seq && paste -d: temp/seq temp/acc | sort -u > temp/blastSeqAcc.temp
-awk '{printf "%s:%s\n", $1, $2}' $1 | sort -u  > temp/diamondSeqAcc.temp
+cut -d\| -f2 $blastDataFile > temp/acc && cut -f1 $blastDataFile >\
+	 temp/seq && paste -d: temp/seq temp/acc | sort -u >\
+	 temp/blastSeqAcc.temp
+awk '{printf "%s:%s\n", $1, $2}' $diamondDataFile | sort -u  >\
+	 temp/diamondSeqAcc.temp
 
 # join on entire line, then select the query sequence (awk),
 # then count unique sequences
 # Queries Mapped by Both Methods
-commonSeq=$(join temp/diamondSeqAcc.temp temp/blastSeqAcc.temp | awk -F ":" '{print $1}' | sort -u | wc -l)
+commonSeq=$(join temp/diamondSeqAcc.temp temp/blastSeqAcc.temp |\
+	awk -F ":" '{print $1}' | sort -u | wc -l)
 
 # Percentage of Queries Mapped
 percQueryMapped=$(echo "scale=3; $commonSeq/$blastUniqQuery" | bc ) 
